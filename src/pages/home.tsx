a479864a5c8d387adb0259dbe0159da9fb47b78b
isakoman <isakoman@layer.agency>
2024-10-12 22:45:44 +0200

    initial commit

---
+++ src/pages/home.tsx
@@ -0,0 +1,17 @@
+import Artzone from "../assets/svg/Subtract";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home">
+        <Artzone className="home__img" />
+        <div className="home__text">
+          <div className="home__title type--orbitron">ARTZONE</div>
+          <div className="home__subtitle">Coming soon</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
