bc171796b1125a62bebbae8eaad892c9540a9799
isakoman <isakoman@layer.agency>
2024-10-13 11:04:03 +0200

    add message

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,5 @@
-import Artzone from "../assets/svg/Subtract";
+import Artzone from "../assets/svg/artzone";
+import ParallaxText from "../components/parallax-text";
 
 const Home = () => {
   return (
@@ -7,9 +8,13 @@ const Home = () => {
         <Artzone className="home__img" />
         <div className="home__text">
           <div className="home__title type--orbitron">ARTZONE</div>
-          <div className="home__subtitle">Coming soon</div>
+          <div className="home__subtitle">- Coming soon -</div>
         </div>
       </div>
+      <section className="parallax__wrapper">
+        <ParallaxText baseVelocity={-3}>Coming soon</ParallaxText>
+        <ParallaxText baseVelocity={3}>Print i Hidrografija</ParallaxText>
+      </section>
     </>
   );
 };
